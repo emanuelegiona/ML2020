ab5bf2db66662f5949950caa7f3d72da5152b666
emanuelegiona <giona.emanuele@gmail.com>
2019-12-05 18:44:05 +0100

    grid parameter search implemented; model selection

--- HW2/src/train.py
+++ HW2/src/train.py
@@ -2,13 +2,15 @@
 Model training and parameter tuning.
 """
 
+from time import sleep
+from math import ceil
 from datetime import datetime
 import numpy as np
-import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 from keras import Model
 from keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
+from typing import Tuple
 
 from src.models import FeatureExtractor, EmaNet, TransferNet
 from src.preprocess import load_dataset
@@ -63,7 +65,9 @@ def kfold_cross_validation(dataset_inputs: np.ndarray, dataset_labels: np.ndarra
 
     history = []
     model.save_weights("../misc/cv/{model_id}.h5".format(model_id=model_id))
-    for _ in range(k_folds):
+    for i in range(1, k_folds+1):
+        print("{m} | Iteration {i}/{k}".format(m=model_id, i=i, k=k_folds))
+
         # start with fresh weights
         model.load_weights("../misc/cv/{model_id}.h5".format(model_id=model_id))
 
@@ -76,7 +80,7 @@ def kfold_cross_validation(dataset_inputs: np.ndarray, dataset_labels: np.ndarra
         model.fit(x=train_inputs, y=train_labels, batch_size=batch_size,
                   validation_data=(test_inputs, test_labels), epochs=epochs,
                   callbacks=[EarlyStopping(monitor="val_acc", patience=10)],
-                  verbose=2)
+                  verbose=0)
 
         # evaluate it
         _, val_acc = model.evaluate(x=test_inputs, y=test_labels, verbose=1)
@@ -87,10 +91,106 @@ def kfold_cross_validation(dataset_inputs: np.ndarray, dataset_labels: np.ndarra
 
 
 def grid_search(dataset_inputs: np.ndarray, dataset_labels: np.ndarray,
-                batch_size: int, epochs: int = 100, k_folds: int = 5) -> None:
+                input_shape: Tuple[int, int], num_classes: int, batch_size: int, log_path: str,
+                epochs: int = 100, k_folds: int = 5) -> None:
 
-    # TODO
-    pass
+    # tuning parameters for an EmaNet model
+    emanet_params = {"convolutions": [], #[1, 2, 3],
+                     "dense_layers": [], #[3, 5, 7],
+                     "starting_filters": [], #[32],
+                     "dropout": []} #[0.3, 0.4]}
+
+    # tuning parameters for a TransferNet model
+    transfernet_params = {"feature_extractor": ["NASNetLarge"], #["Dense121", "Dense169", "Dense201", "NASNetMobile"],
+                          "dense_layers": [3, 5],
+                          "dropout": [0.3, 0.4]}
+
+    # holds scores of each model
+    scores = {}
+
+    with open(log_path, "w"):
+        pass
+
+    with open(log_path, "a") as log:
+        log_message(file_handle=log,
+                    message="Starting grid search...")
+
+        # EmaNet search
+        for conv in emanet_params["convolutions"]:
+            for dense in emanet_params["dense_layers"]:
+                for filters in emanet_params["starting_filters"]:
+                    for prob in emanet_params["dropout"]:
+                        model_id = "EmaNet_c{c}_d{d}_f{f}_p{p}".format(c=conv,
+                                                                       d=dense,
+                                                                       f=filters,
+                                                                       p=prob)
+                        log_message(file_handle=log,
+                                    message="Testing model: {m}".format(m=model_id))
+
+                        model = EmaNet(input_shape=input_shape,
+                                       num_classes=num_classes,
+                                       convolutions=conv,
+                                       dense_layers=dense,
+                                       starting_filters=filters,
+                                       dropout_rate=prob)
+
+                        min_acc, mean_acc, std_acc, max_acc = kfold_cross_validation(dataset_inputs=dataset_inputs,
+                                                                                     dataset_labels=dataset_labels,
+                                                                                     model_id=model_id,
+                                                                                     model=model,
+                                                                                     batch_size=batch_size,
+                                                                                     epochs=epochs,
+                                                                                     k_folds=k_folds)
+
+                        scores[model_id] = mean_acc
+                        res = "Min/Avg (std)/Max accuracy: {v1:.3f} / {v2:.3f} ({v3:.3f}) / {v4:.3f}".format(v1=min_acc,
+                                                                                                             v2=mean_acc,
+                                                                                                             v3=std_acc,
+                                                                                                             v4=max_acc)
+
+                        log_message(file_handle=log, message=res)
+                        #sleep(60)
+
+        # TransferNet search
+        for fe in transfernet_params["feature_extractor"]:
+            for dense in transfernet_params["dense_layers"]:
+                for prob in transfernet_params["dropout"]:
+                    model_id = "TransferNet_{fe}_d{d}_p{p}".format(fe=fe,
+                                                                   d=dense,
+                                                                   p=prob)
+                    log_message(file_handle=log,
+                                message="Testing model: {m}".format(m=model_id))
+
+                    model = TransferNet(input_shape=input_shape,
+                                        num_classes=num_classes,
+                                        feature_extractor=FeatureExtractor.get(name=fe),
+                                        dense_layers=dense,
+                                        dropout_rate=prob)
+
+                    min_acc, mean_acc, std_acc, max_acc = kfold_cross_validation(dataset_inputs=dataset_inputs,
+                                                                                 dataset_labels=dataset_labels,
+                                                                                 model_id=model_id,
+                                                                                 model=model,
+                                                                                 batch_size=batch_size,
+                                                                                 epochs=epochs,
+                                                                                 k_folds=k_folds)
+
+                    scores[model_id] = mean_acc
+                    res = "Min/Avg (std)/Max accuracy: {v1:.3f} / {v2:.3f} ({v3:.3f}) / {v4:.3f}".format(v1=min_acc,
+                                                                                                         v2=mean_acc,
+                                                                                                         v3=std_acc,
+                                                                                                         v4=max_acc)
+
+                    log_message(file_handle=log, message=res)
+                    #sleep(60)
+
+        log_message(file_handle=log,
+                    message="Grid search ended.\n\nResults:\nModel\t\tMean accuracy\n")
+
+        for k in sorted(scores, key=scores.get, reverse=True):
+            log_message(file_handle=log,
+                        message="{k}\t{v}".format(k=k, v=scores[k]),
+                        with_time=False)
 
 
 if __name__ == "__main__":
@@ -108,19 +208,10 @@ if __name__ == "__main__":
     # whole dataset
     inputs, labels = load_dataset(train_generator=train_gen)
 
-    # model
-    my_model = EmaNet(input_shape=train_gen.image_shape,
-                      num_classes=train_gen.num_classes)
-
-    # k-fold CV
-    min_acc, mean_acc, std_acc, max_acc = kfold_cross_validation(dataset_inputs=inputs,
-                                                                 dataset_labels=labels,
-                                                                 model_id="EmaNet_base",
-                                                                 model=my_model,
-                                                                 batch_size=batch_size,
-                                                                 epochs=50)
-
-    print("Min/Avg +- std/Max accuracy: {v1:.3f} / {v2:.3f} +- {v3:.3f} / {v4:.3f}".format(v1=min_acc,
-                                                                                           v2=mean_acc,
-                                                                                           v3=std_acc,
-                                                                                           v4=max_acc))
+    grid_search(dataset_inputs=inputs,
+                dataset_labels=labels,
+                input_shape=train_gen.image_shape,
+                num_classes=train_gen.num_classes,
+                batch_size=batch_size,
+                log_path="../misc/grid_search2.txt",
+                epochs=50)
